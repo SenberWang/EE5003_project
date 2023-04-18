5603441be7d1baa0ee84b1e02f84aa0ff23c4259
XINGBO WANG <64885555+SenberWang@users.noreply.github.com>
2023-04-18 21:36:00 +0800

    Add files via upload

---
+++ Data_preparation_tools/Compiler/cmake-build-debug/CMakeFiles/CL_compiler.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/EE5003/my_work/tool/CL_compiler_v3/assembly.cpp" "CMakeFiles/CL_compiler.dir/assembly.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/assembly.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/global_data.cpp" "CMakeFiles/CL_compiler.dir/global_data.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/global_data.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/handler.cpp" "CMakeFiles/CL_compiler.dir/handler.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/handler.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/init.cpp" "CMakeFiles/CL_compiler.dir/init.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/init.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/machine_code.cpp" "CMakeFiles/CL_compiler.dir/machine_code.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/machine_code.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/main.cpp" "CMakeFiles/CL_compiler.dir/main.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/main.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/num_magic.cpp" "CMakeFiles/CL_compiler.dir/num_magic.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/num_magic.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/operation_code.cpp" "CMakeFiles/CL_compiler.dir/operation_code.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/operation_code.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/token.cpp" "CMakeFiles/CL_compiler.dir/token.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/token.cpp.obj.d"
+  "E:/EE5003/my_work/tool/CL_compiler_v3/translator.cpp" "CMakeFiles/CL_compiler.dir/translator.cpp.obj" "gcc" "CMakeFiles/CL_compiler.dir/translator.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Data_preparation_tools/Compiler/cmake-build-debug/CMakeFiles/CL_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CL_compiler.exe"
+  "CL_compiler.exe.manifest"
+  "CL_compiler.pdb"
+  "CMakeFiles/CL_compiler.dir/assembly.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/assembly.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/global_data.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/global_data.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/handler.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/handler.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/init.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/init.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/machine_code.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/machine_code.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/main.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/main.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/num_magic.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/num_magic.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/operation_code.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/operation_code.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/token.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/token.cpp.obj.d"
+  "CMakeFiles/CL_compiler.dir/translator.cpp.obj"
+  "CMakeFiles/CL_compiler.dir/translator.cpp.obj.d"
+  "libCL_compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CL_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
